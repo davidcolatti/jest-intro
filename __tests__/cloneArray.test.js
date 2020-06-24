dba7ab34f0128f64d5c557b037fc02e3b19ae41d
David Colatti <davidcolatti@gmail.com>
2020-06-24 10:53:34 -0400

    feat: coverage in testing --coverage

--- __tests__/cloneArray.test.js
+++ __tests__/cloneArray.test.js
@@ -2,6 +2,6 @@ const cloneArray = require("../cloneArray");
 
 test("properly cloens array", () => {
   const array = [1, 2, 3];
-  expect(cloneArray(array)).toEqual(array); // needs to be toEqual because toBe wont work because it's checking the reference ID
+  expect(cloneArray(array)).toEqual(array); // checking to see if the array is the same
   expect(cloneArray(array)).not.toBe(array); // testing to see if array is making a copy and not the same array
 });
